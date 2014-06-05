55903c5ff8806959c23b137a4eab8ff893acc8dc
Steve McDaniel <radarseesradar@gmail.com>
2014-06-05 12:44:27 -0600

    Fix tests.

--- spec/models/spree/bank_spec.rb
+++ spec/models/spree/bank_spec.rb
@@ -2,17 +2,16 @@ require 'spec_helper'
 
 describe Spree::Bank do
   it { should allow_mass_assignment_of :name }
-  it { should allow_mass_assignment_of :account_no }
+  it { should allow_mass_assignment_of :instructions }
   it { should allow_mass_assignment_of :active }
   it { should validate_presence_of :name }
-  it { should validate_presence_of :account_no }
-  it { should validate_uniqueness_of(:account_no).scoped_to(:name) }
+  it { should validate_presence_of :instructions }
 
   describe ".active" do
     before(:each) do
-      @active_bank1 = Spree::Bank.create! :name => 'bank1', :account_no => '98765'
-      @active_bank2 = Spree::Bank.create! :name => 'bank2', :account_no => '98765'
-      @bank3 = Spree::Bank.create! :name => 'bank3', :account_no => '98765', :active => false
+      @active_bank1 = Spree::Bank.create! :name => 'bank1', :instructions => '98765'
+      @active_bank2 = Spree::Bank.create! :name => 'bank2', :instructions => '98765'
+      @bank3 = Spree::Bank.create! :name => 'bank3', :instructions => '98765', :active => false
     end
 
     it "returns active banks" do
